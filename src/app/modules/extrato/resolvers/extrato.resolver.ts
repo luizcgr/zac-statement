7a96a292f99280ada84cf33cdd6e64081dfbd12d
@luizcgr <luizcgr@gmail.com>
2025-10-08 15:38:59 -0300

    Refactor Extrato component layout for improved alignment and consistency

--- src/app/modules/extrato/resolvers/extrato.resolver.ts
+++ src/app/modules/extrato/resolvers/extrato.resolver.ts
@@ -3,7 +3,6 @@ import { ResolveFn } from '@angular/router';
 import { ExtratoService } from '../services/extrato.service';
 
 export const extratoResolver: ResolveFn<unknown> = (snapshot) => {
-  console.log('resolving...');
   const extratoService = inject(ExtratoService);
   const cartao = snapshot.paramMap.get('cartao')!;
   return extratoService.consultar(cartao);
