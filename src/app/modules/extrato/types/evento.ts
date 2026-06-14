4eba02d575ed09777de062f06ba7ede1809487ca
@luizcgr <luizcgr@gmail.com>
2026-06-14 11:39:55 -0300

    refactor: remove unused properties from Evento and Extrato types

--- src/app/modules/extrato/types/evento.ts
+++ src/app/modules/extrato/types/evento.ts
@@ -3,12 +3,6 @@ import { Patrocinador } from "./patrocinador";
 export type Evento = {
   id: number;
   nome: string;
-  inicio: Date;
-  fim: Date;
-  ativo: boolean;
-  modoDebito: boolean;
-  pausado?: boolean;
-  urlCardapio?: string;
   chave: string;
   patrocinadores: Patrocinador[];
 };
--- src/app/modules/extrato/types/extrato.ts
+++ src/app/modules/extrato/types/extrato.ts
@@ -3,19 +3,10 @@ import { ItemExtrato } from './item-extrato';
 import { Pedido } from './pedido';
 
 export type Extrato = {
-  cartaoId: number;
   codigo: string;
   saldo: number;
-  dataLiberacao: Date;
-  inativacao: Date;
-  usuarioInativacao?: string;
-  ativo: boolean;
   emUso: boolean;
-  usuario: string;
-  data: Date;
   evento: Evento;
   historico: ItemExtrato[];
-  tag?: string;
-  recargaUnica: boolean;
   pedidos: Pedido[];
 };
